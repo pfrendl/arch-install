1622cac632cec57798040b15f2de7253336f76c2
pfrendl <pfre00@gmail.com>
2023-10-20 03:43:26 +0200

    statusbar changed to dwmblocks

--- configure_system.sh
+++ configure_system.sh
@@ -35,14 +35,16 @@ pacman --noconfirm -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# dwm dependencies
-pacman --noconfirm -S xorg-server xorg-xinit libxft libxinerama
+# dwm dependencies and xbindkeys for audio keys
+pacman --noconfirm -S xorg-server xorg-xinit libxft libxinerama xbindkeys
 # fonts ~ inconsolata: xresources, fontawesome: icons, noto-fonts: unicode & emoji
 pacman --noconfirm -S ttf-inconsolata ttf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-cjk
 # multimedia
 pacman --noconfirm -S xwallpaper nsxiv pulseaudio ffmpeg yt-dlp mpv
+# graphics driver
+pacman --noconfirm -S nvidia
 # misc
-pacman --noconfirm -S vim less git neofetch
+pacman --noconfirm -S vim less git neofetch man
 
 # browser
 sudo -u $user mkdir /home/$user/aur && cd $_
@@ -51,10 +53,17 @@ cd brave-bin
 sudo -u $user makepkg -si --noconfirm
 
 # suckless software
+suckless_repos=(
+    https://github.com/pfrendl/dwm.git
+    https://github.com/pfrendl/st.git
+    https://github.com/pfrendl/dmenu.git
+    https://github.com/pfrendl/dwmblocks.git
+)
 sudo -u $user mkdir /home/$user/suckless && cd $_
-for program in dwm st dmenu slstatus
+for repo in "${suckless_repos[@]}"
 do
-    sudo -u $user git clone https://github.com/pfrendl/$program.git
+    sudo -u $user git clone $repo
+    program=$(basename $repo .git)
     cd $program
     make clean install
     cd ..
